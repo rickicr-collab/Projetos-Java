052434d2af89002bfee4cdac8046b2cbe088ffce
rickicr-collab <ricardo_rosendo@ymail.com>
2024-01-17 00:15:11 -0300

    Commit sobre operadores Aritmeticos

---
+++ OperadoresAritmeticos/src/OperadoresA.java
@@ -0,0 +1,18 @@
+
+public class OperadoresA {
+	
+	/*
+	 * operação simples usando variaveis com valores
+	 * atribuios e impressos na tela.
+	 */
+
+	public static void main(String[] args) {
+		int n1 = 5;
+		int n2 = 2;
+		float m = (n1 + n2) / 2;
+		
+		System.out.println("A média da operação acima é :" +  m);
+
+	}
+
+}
